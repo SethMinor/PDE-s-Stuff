75286d7c53721cf744e5484ce560e1de47472641
Seth Minor <97004318+SethMinor@users.noreply.github.com>
2022-03-15 14:32:03 -0700

    Add files via upload

---
+++ WaveMovie.m
@@ -0,0 +1,94 @@
+clear, clc, clf;
+
+% rectangular spatial domain parameters
+L = 10; % length
+H = 5; % height
+C = 0.15; % wave speed
+
+% temporal parameters
+tf = 100; % final time
+num_frames = 500; % number of movie frames
+time = linspace(0,tf,num_frames); % time span
+
+% other parameters
+p = 5.1;
+q = p;
+
+% spatial eigenfunctions 
+phi =@(n,m,x,y) sin(((n*pi)/L).*x).*cos(((m*pi)/H).*y); % \phi_{n,m}
+
+% temporal part of solution 
+lambda =@(n,m) (n*pi/L)^2 + (m*pi/H)^2;% eigenvalues
+h =@(n,m,t) sin(sqrt(lambda(n,m))*C*t);
+
+% creating the spatial mesh 
+nx = 60; % resolution of the mesh
+ny = 60;
+
+rows = 5; % n's
+columns = 5; % m's
+
+x = linspace(0,L,nx);
+y = linspace(0,H,nx);
+[X,Y] = meshgrid(x,y);
+
+% defining the coefficients (given L,H,p,q)
+cn =@(n) (4*L*p^2*sin(n*pi/2)*sin((n*pi)/(2*p)))/(n*pi*(4*p^2-n^2)); 
+cm =@(m) (4*H*q^2*sin(m*pi/2)*sin((m*pi)/(2*q)))/(m*pi*(4*q^2-m^2));
+Anm =@(n,m) ((pi^2)/(L*H))*cn(n)*cm(m);
+
+%% create the movie by plotting the sum of the spatial modes
+figgy = figure;
+
+axis = gca; 
+colormap(axis,autumn(30))
+axis.NextPlot = 'replaceChildren';
+grid on
+bagOframes(num_frames) = struct('cdata',[],'colormap',[]);
+
+%V = VideoWriter('Waving_membrane.avi');
+%open(V);
+
+% increment over time
+j = 1;
+for t=time
+
+    U = phi(1,0,X,Y); % initialize the solution
+
+    % increment over space
+    for n = 1:rows % n's
+        for m = 1:columns % m's
+            A_nm = Anm(n,m); % compute coefficients
+            
+            phi_nm = phi(n,m,X,Y); % spatial modes
+            h_nm = h(n,m,t); % temporal solution
+
+            U = U + phi_nm*h_nm; % add another (n,m) mode
+        end
+    end
+
+    % plot solution at time t
+    surf(X,Y,U)
+    zlim([-15 15])
+    subtitle("Time: " + t,'interpreter','latex')
+    xlabel('$x$ (for $L=10$)','interpreter','latex')
+    ylabel('$y$ (for $H=5$)','interpreter','latex')
+    zlabel('$u = u(x,y,t)$','interpreter','latex')
+    drawnow
+    pause(0.3)
+    
+    frame = getframe(axis);
+    bagOframes(j) = frame;
+    
+    %writeVideo(V,frame);
+
+    j = j + 1;
+
+end
+
+close(V);
+
+% play the movie
+movie(bagOframes,1,30);
+
+
